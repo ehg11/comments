2f9de6c709958e9b50078d6df6805adfa442ace6
ehg <hwang.edward8@gmail.com>
2022-08-16 13:12:55 -0700

    Initial Page Setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import surtr from "./assets/surtrbruh.png"
 
-function App() {
+export default function App() {
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <body className="bg-slate-300 w-screen h-screen flex flex-col justify-start items-center">
+      <div className="bg-slate-400 shadow-lg w-full h-24 px-5 flex flex-row items-center">
+        <div className="font-mono text-2xl tracking-wide font-bold grow">
+          Bookmarks
+        </div>
+        <img src={surtr} alt="surtr" className="w-16 h-16 rounded-full items-end hover:brightness-90" />
+      </div>
+      <div className="w-full h-full flex justify-center items-center">
+        <div className="bg-white w-11/12 h-5/6 p-10 rounded-xl shadow-lg">
+          
+        </div>
+      </div>
+    </body>
+  )
 }
-
-export default App;
