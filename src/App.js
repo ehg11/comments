a50f841d12d807898c4156d907ecc93984a468f3
ehg <hwang.edward8@gmail.com>
2022-08-16 15:28:27 -0700

    Initial Layout of Card
    TODO: Color Theme

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import surtr from "./assets/surtrbruh.png"
+import surtr from "./assets/surtr-sq.png";
+import { TextParagraph } from "@styled-icons/bootstrap";
+import Card from "./Card.js"
 
 export default function App() {
 
-  
+    const goToLink = (link) => {
+        window.open(link, "_blank");
+    };
 
-  return (
-    <body className="bg-slate-300 w-screen h-screen flex flex-col justify-start items-center">
-      <div className="bg-slate-400 shadow-lg w-full h-24 px-5 flex flex-row items-center">
-        <div className="font-mono text-2xl tracking-wide font-bold grow">
-          Bookmarks
+    return (
+        <div className="bg-slate-300 w-screen h-screen flex flex-col justify-start items-center">
+        <div className="bg-slate-400 shadow-lg w-full h-24 px-5 flex flex-row items-center">
+            <TextParagraph className="h-12 w-12 mr-5"/>
+            <div className="font-sorabold text-2xl tracking-widest font-bold grow">
+                Bookmarks
+            </div>
+            <button onClick={() => goToLink("https://github.com/ehg11/comments")}>
+                <img src={surtr} alt="surtr" 
+                    className="w-16 h-16 rounded-full items-end hover:brightness-90 shadow-xl" 
+                />
+            </button>
+            </div>
+            <div className="w-full h-full flex justify-center items-center">
+                <div className="bg-white w-11/12 h-5/6 p-10 rounded-xl shadow-lg">
+                    <Card />
+                </div>
+            </div>
         </div>
-        <img src={surtr} alt="surtr" className="w-16 h-16 rounded-full items-end hover:brightness-90" />
-      </div>
-      <div className="w-full h-full flex justify-center items-center">
-        <div className="bg-white w-11/12 h-5/6 p-10 rounded-xl shadow-lg">
-          
-        </div>
-      </div>
-    </body>
-  )
+    )
+
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,24 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    minHeight: {
+      '0': '0',
+      '1/4': '25%',
+      '1/3': '33%',
+      '1/2': '50%',
+      '3/4': '75%',
+      'full': '100%',
+    },
+    extend: {
+      fontFamily: {
+        sorabold: ["Sora-Bold", "sans-serif"],
+        sora: ["Sora", "sans-serif"],
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    function ({ addVariant }) {
+      addVariant("child", "& > *")
+    }
+  ],
 }
