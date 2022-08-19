979ecfbff9f470719e10276fcf9cf166fad659f7
ehg <hwang.edward8@gmail.com>
2022-08-18 18:00:23 -0700

    Intermediate Commit - Features not Complete
    TODO: Find way to have multiple submit/cancel button
    Clear stateful on cancel
    Figure out Spacing for Forms

--- src/App.js
+++ src/App.js
@@ -1,56 +1,79 @@
 import surtr from "./assets/surtr-sq.png";
-import { TextParagraph } from "@styled-icons/bootstrap";
+import { TextParagraph, PlusLg } from "@styled-icons/bootstrap";
 import styled from 'styled-components';
 import { ACTIONS, test_card, colors } from './utils.js'
 import Card from "./Card.js"
 import { useReducer } from "react"
 
+
 const StyledIcon = styled(TextParagraph)`
     color: ${colors.light};
 `
-
-function reducer(cards, action) {
-    switch(action.type) {
-        case ACTIONS.REMOVE:
-            return cards.filter(card => card.id !== action.payload.id)
-        case ACTIONS.STAR:
-            return cards.map(card => {
-                if (card.id === action.payload.id) {
-                    return { ...card, starred: !card.starred};
-                }
-                return card;
-            })
-        case ACTIONS.UPVOTE:
-            return cards.map(card => {
-                if (card.id === action.payload.id) {
-                    return { ...card, score: card.score + 1};
-                }
-                return card;
-            })
-        case ACTIONS.DOWNVOTE:
-            return cards.map(card => {
-                if (card.id === action.payload.id) {
-                    return { ...card, score: card.score - 1};
-                }
-                return card;
-            })
-        default:
-            return cards;
-    }
-}
-
 
 export default function App() {
 
     const test_cards = [test_card]
 
     const [all_cards, dispatch] = useReducer(reducer, test_cards);
+
+    function reducer(cards, action) {
+        switch(action.type) {
+            case ACTIONS.SUBMIT:
+                return cards.map(card => {
+                    if (card.id === action.payload.id) {
+                        return { ...card, title: action.payload.title, body: action.payload.body, finalized: true };
+                    }
+                    return card;
+                })
+            case ACTIONS.ADD_NEWCARD:
+                if (cards.find(card => !card.finalized)) {
+                    return cards;
+                }
+                return [emptyCard(), ...cards]
+            case ACTIONS.REMOVE:
+                return cards.filter(card => card.id !== action.payload.id)
+            case ACTIONS.STAR:
+                return cards.map(card => {
+                    if (card.id === action.payload.id) {
+                        return { ...card, starred: !card.starred};
+                    }
+                    return card;
+                })
+            case ACTIONS.UPVOTE:
+                return cards.map(card => {
+                    if (card.id === action.payload.id) {
+                        return { ...card, score: card.score + 1};
+                    }
+                    return card;
+                })
+            case ACTIONS.DOWNVOTE:
+                return cards.map(card => {
+                    if (card.id === action.payload.id) {
+                        return { ...card, score: card.score - 1};
+                    }
+                    return card;
+                })
+            default:
+                return cards;
+        }
+    }
     
-    const goToLink = (link) => {
+    function goToLink(link) {
         window.open(link, "_blank");
     };
 
-    const showCards = (cards) => {
+    function emptyCard() {
+        return {
+            id: Date.now(),
+            title: "",
+            body: "",
+            score: 0,
+            starred: false,
+            finalized: false,
+        }
+    }
+
+    function showCards(cards) {
         return cards.map((card, index) => {
             return (
                 <Card
@@ -62,21 +85,29 @@ export default function App() {
         })
     }
 
+    console.log(all_cards)
+
     return (
         <div className="bg-dark_accent w-screen h-screen flex flex-col justify-start items-center">
-        <div className="bg-dark shadow-lg w-full h-24 px-5 flex flex-row items-center">
-            <StyledIcon className="h-12 w-12 mr-5"/>
-            <div className="font-sorabold text-2xl tracking-widest font-bold grow text-light">
-                Bookmarks
+            <div className="bg-dark shadow-lg w-full h-24 px-5 flex flex-row items-center opacity-90">
+                <StyledIcon className="h-12 w-12 mr-5"/>
+                <div className="font-sorabold text-2xl tracking-widest font-bold grow text-light">
+                    Bookmarks
+                </div>
+                <button onClick={() => goToLink("https://github.com/ehg11/comments")}>
+                    <img src={surtr} alt="surtr" 
+                        className="w-12 h-12 rounded-full items-end hover:brightness-90 shadow-xl mx-4" 
+                    />
+                </button>
             </div>
-            <button onClick={() => goToLink("https://github.com/ehg11/comments")}>
-                <img src={surtr} alt="surtr" 
-                    className="w-16 h-16 rounded-full items-end hover:brightness-90 shadow-xl" 
-                />
-            </button>
-            </div>
-            <div className="w-full h-full flex justify-center items-center">
-                <div className="bg-white w-11/12 h-5/6 p-10 rounded-xl shadow-lg flex flex-col gap-8">
+            <div className="w-full h-full flex justify-center items-top py-12 overflow-y-auto scroll">
+                <div className="bg-white w-11/12 min-h-full h-fit p-12 rounded-xl shadow-lg flex flex-col gap-8">
+                    <button 
+                        className="bg-light p-2 rounded-2xl drop-shadow-md border-2 border-light hover:border-light_accent h-12 flex items-center justify-center"
+                        onClick={() => dispatch({ type: ACTIONS.ADD_NEWCARD })}
+                    >
+                        <PlusLg className="h-6"/>
+                    </button>
                     {showCards(all_cards)}
                 </div>
             </div>
--- src/Card.js
+++ src/Card.js
@@ -2,6 +2,7 @@ import { X, Plus, ArrowUpShort, ArrowDownShort, Star, StarFill } from "@styled-i
 import { ModeEdit } from "@styled-icons/material"
 import styled from 'styled-components';
 import { ACTIONS, colors } from "./utils.js";
+import { useState, useRef, useEffect } from "react";
 
 const StyledX = styled(X)`
     &:hover {
@@ -20,51 +21,168 @@ export default function Card({ card, dispatch }) {
     const score = card.score ?? 0;
     const starred = card.starred ?? false;
 
-    return (
-        <div className="flex flex-col w-full h-fit min-h-1/3 rounded-2xl align-top overflow-hidden child:px-5 drop-shadow-lg">
-            <div className="bg-light h-fit min-h-16 w-full flex items-center gap-1 pt-5">
-                <span className="flex-grow font-sorabold text-2xl text-important mr-1"> {title} </span>
-                <button 
-                    className="hover:brightness-90 w-fit h-fit bg-inherit rounded-full flex items-center self-start"
-                    onClick={() => dispatch({ type: ACTIONS.STAR, payload: {id: id}})}
-                >
-                    { !starred
-                        ? <Star className="h-6 w-6 p-1" />
-                        : <StyledStarFill className="h-6 w-6 p-1" />
-                    }
-                </button>
-                <button 
-                    className="hover:brightness-90 w-fit h-fit bg-inherit rounded-full flex items-center self-start"
-                    onClick={() => dispatch({ type: ACTIONS.REMOVE, payload: {id: id}})}
-                >
-                    <StyledX className="h-6 w-6" />
-                </button>
+    const [new_title, set_new_title] = useState("");
+    const [new_body, set_new_body] = useState(""); 
+    const [error, set_error] = useState("");
+
+    const titleRef = useRef(null);
+    const bodyRef = useRef(null);
+
+    useEffect(() => {
+        if (titleRef && titleRef.current) {
+            titleRef.current.style.height = "0px";
+            const scrollHeight = titleRef.current.scrollHeight;
+            titleRef.current.style.height = `${scrollHeight + 3}px`;
+        }
+    }, [new_title])
+    
+    useEffect(() => {
+        if (bodyRef && bodyRef.current) {
+            bodyRef.current.style.height = "0px";
+            const scrollHeight = bodyRef.current.scrollHeight;
+            bodyRef.current.style.height = `${scrollHeight + 3}px`;
+        }
+    }, [new_body])
+    
+    function handleSubmit(e) {
+        e.preventDefault();
+        if (new_title === "") {
+            set_error("Please Enter a Title");
+            setTimeout(() => {
+                set_error("");
+            }, 2000);
+        }
+        else if (new_body === "") {
+            set_error("Please Enter a Comment");
+            setTimeout(() => {
+                set_error("");
+            }, 2000);
+        }
+        else {
+            dispatch({ type: ACTIONS.SUBMIT, payload: {id: id, title: new_title, body: new_body }})
+            set_new_title("");
+            set_new_body("");
+        }
+    }
+
+    function cancelEdit() {
+
+    }
+
+    if (card.finalized) {
+        return (
+            <div className="flex flex-col w-full h-fit min-h-1/3 rounded-2xl align-top overflow-hidden child:px-5 drop-shadow-lg">
+                <div className="bg-light h-fit min-h-16 w-full flex items-center gap-1 pt-5">
+                    <span className="flex-grow font-sorabold text-2xl text-important mr-1"> {title} </span>
+                    <button 
+                        className="hover:brightness-90 w-fit h-fit bg-inherit rounded-full flex items-center self-start"
+                        onClick={() => dispatch({ type: ACTIONS.STAR, payload: {id: id}})}
+                    >
+                        { !starred
+                            ? <Star className="h-6 w-6 p-1" />
+                            : <StyledStarFill className="h-6 w-6 p-1" />
+                        }
+                    </button>
+                    <button 
+                        className="hover:brightness-90 w-fit h-fit bg-inherit rounded-full flex items-center self-start"
+                        onClick={() => dispatch({ type: ACTIONS.REMOVE, payload: {id: id}})}
+                    >
+                        <StyledX className="h-6 w-6" />
+                    </button>
+                </div>
+                <div className="bg-light flex-grow h-fit py-4 font-sora text-primary text-sm">
+                    {body}
+                </div>
+                <div className="bg-light_accent h-8 flex items-center">
+                    <button className="hover:brightness-90 w-fit h-fit bg-inherit rounded-full flex items-center mr-2">
+                        <Plus className="h-6 w-6" />
+                    </button>
+                    <button className="hover:brightness-90 w-fit h-fit bg-inherit rounded-full flex items-center">
+                        <ModeEdit className="h-6 w-6 p-1" />
+                    </button>
+                    <span className="flex-grow" />
+                    <span className="font-mono px-2"> {score} </span>
+                    <button 
+                        className="hover:brightness-90 w-fit h-fit bg-inherit rounded-full flex items-center"
+                        onClick={() => dispatch({ type: ACTIONS.UPVOTE, payload: { id: id }})}
+                    >
+                            <ArrowUpShort className="h-6 w-6" />
+                    </button>
+                    <button 
+                        className="hover:brightness-90 w-fit h-fit bg-inherit rounded-full flex items-center"
+                        onClick={() => dispatch({ type: ACTIONS.DOWNVOTE, payload: {id: id }})}
+                    >
+                        <ArrowDownShort className="h-6 w-6" />
+                    </button>
+                </div>
             </div>
-            <div className="bg-light flex-grow h-fit py-4 font-sora text-primary text-sm">
-                {body}
-            </div>
-            <div className="bg-light_accent h-8 flex items-center">
-                <button className="hover:brightness-90 w-fit h-fit bg-inherit rounded-full flex items-center mr-2">
-                    <Plus className="h-6 w-6" />
-                </button>
-                <button className="hover:brightness-90 w-fit h-fit bg-inherit rounded-full flex items-center">
-                    <ModeEdit className="h-6 w-6 p-1" />
-                </button>
-                <span className="flex-grow" />
-                <span className="font-mono px-2"> {score} </span>
-                <button 
-                    className="hover:brightness-90 w-fit h-fit bg-inherit rounded-full flex items-center"
-                    onClick={() => dispatch({ type: ACTIONS.UPVOTE, payload: { id: id }})}
-                >
-                        <ArrowUpShort className="h-6 w-6" />
-                </button>
-                <button 
-                    className="hover:brightness-90 w-fit h-fit bg-inherit rounded-full flex items-center"
-                    onClick={() => dispatch({ type: ACTIONS.DOWNVOTE, payload: {id: id }})}
-                >
-                    <ArrowDownShort className="h-6 w-6" />
-                </button>
-            </div>
-        </div>
-    )
+        )
+    }
+    else {
+        return (
+            <form 
+                className={`flex flex-col w-full h-fit min-h-1/3 rounded-2xl align-top overflow-hidden child:px-5 drop-shadow-lg ${error ? "shake" : ""}`}
+                onSubmit={ handleSubmit }
+            >
+                <div className="bg-light h-fit w-full flex items-center gap-1 pt-5">
+                    <div className="flex-grow font-sorabold text-2xl text-important bg-inherit h-fit">
+                        <textarea ref={ titleRef } value={ new_title } 
+                            placeholder="Give your Comment a Title..."
+                            onChange={e => set_new_title(e.target.value)}
+                            className="w-full max-w-full bg-inherit focus:outline-none resize-none font-sorabold"
+                        />
+                    </div>
+                    <button 
+                        className="hover:brightness-90 w-fit h-fit bg-inherit rounded-full flex items-center self-start"
+                        onClick={() => dispatch({ type: ACTIONS.STAR, payload: {id: id}})}
+                    >
+                        { !starred
+                            ? <Star className="h-6 w-6 p-1" />
+                            : <StyledStarFill className="h-6 w-6 p-1" />
+                        }
+                    </button>
+                    <button 
+                        className="hover:brightness-90 w-fit h-fit bg-inherit rounded-full flex items-center self-start"
+                        onClick={() => dispatch({ type: ACTIONS.REMOVE, payload: {id: id}})}
+                    >
+                        <StyledX className="h-6 w-6" />
+                    </button>
+                </div>
+                <div className="bg-light flex-grow h-fit py-4 font-sora text-primary text-sm">
+                    <textarea ref={ bodyRef } value={ new_body }
+                        placeholder="Add a Comment..."
+                        onChange={e => set_new_body(e.target.value) }
+                        className="w-full bg-inherit focus:outline-none resize-none"
+                    />
+                </div>
+                <div className={`${!error ? "bg-light_accent" : "danger-color" } h-8 flex items-center`}>
+                    <button type="submit" name="submit_button"
+                        className="hover:brightness-90 w-fit h-fit bg-inherit rounded-full flex items-center mr-2 underline px-2 py-0.5 font-sorabold"
+                    >
+                        Submit
+                    </button>
+                    <button type="submit" name="cancel_button" value="Cancel"
+                        className="hover:brightness-90 w-fit h-fit bg-inherit rounded-full flex items-center mr-2 underline px-2 py-0.5 font-sorabold"
+                    >
+                    </button>
+                    <div className="flex-grow flex">
+                        <span className={`font-sorabold ${error ? "fade-text" : ""}`}> { error } </span>
+                    </div>
+                    <span className="font-mono px-2"> {score} </span>
+                    <button 
+                        className="hover:brightness-90 w-fit h-fit bg-inherit rounded-full flex items-center"
+                        onClick={() => dispatch({ type: ACTIONS.UPVOTE, payload: { id: id }})}
+                    >
+                            <ArrowUpShort className="h-6 w-6" />
+                    </button>
+                    <button 
+                        className="hover:brightness-90 w-fit h-fit bg-inherit rounded-full flex items-center"
+                        onClick={() => dispatch({ type: ACTIONS.DOWNVOTE, payload: {id: id }})}
+                    >
+                        <ArrowDownShort className="h-6 w-6" />
+                    </button>
+                </div>
+            </form>
+        )
+    }
 }
